2c96321e076bef3ba04df10a4d91ba7cfe966d2c
codezella-hub <benslimenlouay29@gmail.com>
2024-07-12 14:46:28 +0200

    second upload chnages

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  profile = {
+    firstName : "louay",
+    lastName : "ben slimen",
+    website : "www.linkedin.com",
+    country : "Tunisia"
+  }
+  linkImage = "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp"
+}
